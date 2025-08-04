55e5bb7a71e5517cf17429dfc483bdb739913983
sharofathonmirzayeva3-spec <sharofathonmirzayeva3@gmail.com>
2025-08-04 13:48:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+let a = prompt('ism')
+let b = prompt('fmila')
+let c = prompt('yosh')
+
+
+
+let h1 = document.querySelector('h1')
+let h2 = document.querySelector('h2')
+let h3 = document.querySelector('h3')
+
+
+
+h1.textContent = 'ism:' + a
+h2.textContent = 'fmila:' + b
+h3.textContent = 'yosh:' + c
